63ba96c136f80c2af8ed2cae8e14e13675a8fbba
rderbysh <rderbysh@ualberta.ca>
2017-09-23 16:00:37 -0600

    Got some more of the app design started.

--- rderbysh_CountBook/app/src/main/java/com/example/randi/rderbysh_countbook/MainActivity.java
+++ rderbysh_CountBook/app/src/main/java/com/example/randi/rderbysh_countbook/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.randi.rderbysh_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.randi.rderbysh_countbook.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, CountBookList.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
